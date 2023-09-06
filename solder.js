7648f99c3e38e0383cf29d52b2a5f2819d896643
KhEsanukaev <halid.esanukaev.86@mail.ru>
2023-09-06 11:26:33 +0300

    первый шаг

---
+++ solder.js
@@ -0,0 +1,17 @@
+
+const solder = {
+    name: 'Khalid',
+    health: '10',
+    gun: {
+        nameGun: ak47,
+        numberAmmunition : 30
+    },
+    provisions: '3',
+    
+
+    
+
+  shoot: function () {},
+  recharge: function () {},
+  hurt: function () {},
+} 
