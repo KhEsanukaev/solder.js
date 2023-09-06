791851799a2dcba66be7689dfda53f77234603da
KhEsanukaev <halid.esanukaev.86@mail.ru>
2023-09-06 12:59:13 +0300

    end

--- solder.js
+++ solder.js
@@ -6,12 +6,45 @@ const solder = {
         nameGun: "ak47",
         numberAmmunition : 30
     },
-    provisions: '3',
+    provisions: 3,
     
 
     
 
-  shoot: function () {},
-  recharge: function () {},
-  hurt: function () {},
+  shoot: function () {
+    if (this.gun.numberAmmunition === 0) {
+        return `обойма пуста. Перезаредитесь`
+    } 
+
+
+    this.gun.numberAmmunition--
+
+    console.log(`бах-бах`)
+  },
+  recharge: function () {
+    if (this.provisions === 0) {
+        return `обойма пуста. Перезаредитесь`
+    }
+
+    this.provisions-- 
+    this.gun.numberAmmunition = 30
+
+    console.log(`перезарядка...`)
+
+
+
+
+  },
+  hurt: function () {
+    if (this.health === 0) {
+        
+    }
+    this.health--
+    console.log(`Ты проиграл`);
+  },
 } 
+
+solder.shoot()
+solder.recharge()
+solder.hurt()
+console.log(solder)
